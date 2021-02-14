5c4a05224c5373d213c106da98cdd353adc18854
shinigami-git <75805201+shinigami-git@users.noreply.github.com>
2021-02-14 20:01:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,25 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+	   this.state={
+	   renderPara:false,
+	   }
+	    this.handleClick=()=> {
+	    this.setState({
+	    renderPara:true,
+	    });
+	    };
 	};
 
     render() {
     	return(
     		<div id="main">
+		 {renderPara && <p id="para">
+          {text}
+        </p>}
+    {!renderPara && <button id="click" onClick={this.handleClick}>
+            click me
+          </button>}
 				{ /* Do not remove this main div!! */ }
     		</div>
     	);
